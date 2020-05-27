57bcc0bb7197588f7a7f48e94deb551bf069bdfc
divishrengasamy <48486061+divishrengasamy@users.noreply.github.com>
2020-05-27 15:57:37 +0100

    Fix edge cases

--- ensembleFI.py
+++ ensembleFI.py
@@ -1,4 +1,51 @@
-def ensemble_feature_importance(NUM_FEATS, features, output, coef):
+import string
+import random
+from collections import Counter
+from statistics import mean
+
+import eli5
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly as py
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import plotly.tools as tls
+import shap
+import tensorflow as tf
+import tensorflow_addons as tfa
+import torch
+import torch.nn as nn
+from captum.attr import IntegratedGradients, LayerConductance, NeuronConductance
+from eli5.sklearn import PermutationImportance
+from helper_rank import helper_rank_func
+from majority_vote import majority_vote_func
+from scipy import stats
+from scipy.optimize import minimize
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets import make_regression
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.feature_selection import SelectKBest, mutual_info_regression
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import *
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor
+
+from optimizer import Lookahead
+from radam import RAdam
+
+plt.style.use("seaborn-pastel")
+
+# initiate plotly and cufflinks for offline mode
+py.offline.init_notebook_mode(connected=True)
+#cf.go_offline()
+# initiate Javascript for SHAP
+shap.initjs()
+
+def ensemble_feature_importance(NUM_FEATS, features, output, coef, informative, noise):
     
     # Function to normalise aray
     def normalise(array):
@@ -8,7 +55,13 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
     scaled_coef = normalise(coef)
     
     # give features alphabetical name
-    alphabet_list = list(string.ascii_lowercase)
+    
+    def id_generator(size=5, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    alphabet_list = list()
+    for i in range(1000):
+        alphabet_list.append(id_generator())
     column_names = np.array(alphabet_list[0:NUM_FEATS])
     df_features = pd.DataFrame(features, columns=column_names)
     
@@ -16,15 +69,20 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
     
     mae_scorer = make_scorer(mean_absolute_error, greater_is_better=False)
     # split data to train-test split
-    X_train, X_test, y_train, y_test = train_test_split(features, output, test_size=0.2, shuffle=True, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(features, output, test_size=0.2, shuffle=True)
     
     # fit random forest
     rf_regr = RandomForestRegressor(max_depth=7, n_estimators=700)
     rf_regr.fit(X_train, y_train)
     rf_predicted = rf_regr.predict(X_test)
     
+    # model error
+    rf_mae = np.abs(rf_predicted - y_test).mean()
+    print(f"predicted_shape:{rf_predicted.shape} y_test shape:{y_test.shape}")
+    print(f"rf_mae:{rf_mae}")
+    
     # permutation importance for random forest
-    pi_result_rf = permutation_importance(rf_regr, X_test, y_test, n_repeats=10, random_state=42, scoring=mae_scorer)
+    pi_result_rf = permutation_importance(rf_regr, X_test, y_test, n_repeats=10, scoring=mae_scorer)
     pi_result_rf_scaled = normalise(pi_result_rf.importances_mean)
     pos = np.arange(column_names.size)
 #     plt.figure(figsize=(15, 10))
@@ -39,9 +97,14 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
     gb_regr.fit(X_train, y_train)
     gb_predicted = gb_regr.predict(X_test)
     
+    # model error
+    gb_mae = np.abs(gb_predicted - y_test).mean()
+    print(f"predicted_shape:{gb_predicted.shape} y_test shape:{y_test.shape}")
+    print(f"rf_mae:{gb_mae}")
+    
     # permutation importance for gradient boosted tree
-    pi_result_gb = permutation_importance(gb_regr, X_test, y_test, n_repeats=10, random_state=42, scoring=mae_scorer)
-
+    pi_result_gb = permutation_importance(gb_regr, X_test, y_test, n_repeats=10, scoring=mae_scorer)
+    
     pi_result_gb_scaled = normalise(pi_result_gb.importances_mean)
     pos = np.arange(column_names.size)
 #     plt.figure(figsize=(15, 10))
@@ -147,6 +210,11 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
 
         torch.save(net.state_dict(), "interpret_model.pt")
         
+    #model error
+    test_input_tensor = torch.from_numpy(X_test).type(torch.FloatTensor)
+    test_output = net(test_input_tensor).detach().numpy()
+    print(f"predicted_shape:{test_output.shape} y_test shape:{y_test.shape}")
+    print("dnn_mae:", (np.abs(test_output - y_test)).mean())
     # Intergrated gradient
     ig = IntegratedGradients(net)
     test_input_tensor = torch.from_numpy(X_train).type(torch.FloatTensor)
@@ -216,7 +284,7 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
     )
 
     history = dnn_model.fit(X_train, y_train)
-    perm = PermutationImportance(dnn_model, scoring=mae_scorer, random_state=1).fit(X_train, y_train)
+    perm = PermutationImportance(dnn_model, scoring=mae_scorer).fit(X_train, y_train)
     dnn_pi_df = eli5.explain_weights_df(perm, feature_names=column_names.tolist())
     dnn_pi_df.sort_values(by=["feature"], inplace=True)
     dnn_pi_scaled = normalise(dnn_pi_df["weight"].values)
@@ -287,10 +355,55 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
         )
     )
 
-
+    print(f"rf_sv:{rf_sv_scaled_reshaped}")
+    print("")
+    print(f"rf_pi:{pi_result_rf_scaled_reshaped}")
+    print("")
+    print(f"gb_sv:{gb_sv_scaled_reshaped}")
+    print("")
+    print(f"gb_pi:{pi_result_gb_scaled_reshaped}")
+    print("")
+    print(f"dnn_sv:{dnn_sv_scaled_reshaped}")
+    print("")
+    print(f"dnn_pi:{dnn_pi_scaled_reshaped}")
+    print("")
+    print(f"dnn_ig:{dnn_ig_scaled_reshaped}")
+    print("")
     all_stacked_df = pd.DataFrame(all_stacked, columns=["rf_sv", "rf_pi", "gb_sv", "gb_pi", "dnn_pi", "dnn_sv", "dnn_ig"])
 
     all_stacked_df.to_csv("all_stacked.csv")
+    
+    print(f"RF PI error: {np.abs(pi_result_rf_scaled - scaled_coef).mean()}")
+    print(f"GB PI error: {np.abs(pi_result_gb_scaled - scaled_coef).mean()}")
+    print(f"DNN PI error: {np.abs(dnn_pi_scaled - scaled_coef).mean()}")
+    print(f"RF+GB+DNN PI error: {np.abs(((pi_result_gb_scaled+pi_result_rf_scaled+dnn_pi_scaled)/3) - scaled_coef).mean()}")
+    print("")
+    print(f"RF SV error: {np.abs(rf_sv_scaled - scaled_coef).mean()}")
+    print(f"GB SV error: {np.abs(gb_sv_scaled - scaled_coef).mean()}")
+    print(f"DNN SV error: {np.abs(dnn_sv_scaled - scaled_coef).mean()}")
+    print(f"RF+GB+DNN SV error: {np.abs(((rf_sv_scaled+gb_sv_scaled+dnn_sv_scaled)/3) - scaled_coef).mean()}")
+    print("")
+    print(f"GB PI error: {np.abs(pi_result_gb_scaled - scaled_coef).mean()}")
+    print(f"GB SV error: {np.abs(gb_sv_scaled - scaled_coef).mean()}")
+    print(f"GB PI + GB SV error: {np.abs(((gb_sv_scaled+pi_result_gb_scaled)/2) - scaled_coef).mean()}")
+    print("")
+    print(f"RF PI error: {np.abs(pi_result_rf_scaled - scaled_coef).mean()}")
+    print(f"RF SV error: {np.abs(rf_sv_scaled - scaled_coef).mean()}")
+    print(f"RF PI + RF SV error: {np.abs(((rf_sv_scaled+pi_result_rf_scaled)/2) - scaled_coef).mean()}")
+    print("")
+    print(f"DNN PI error: {np.abs(dnn_pi_scaled - scaled_coef).mean()}")
+    print(f"DNN SV error: {np.abs(dnn_sv_scaled - scaled_coef).mean()}")
+    print(f"DNN IG error: {np.abs(dnn_ig_scaled - scaled_coef).mean()}")
+    print(f"DNN PI + DNN SV + DNN IG error: {np.abs(((dnn_pi_scaled+dnn_sv_scaled+dnn_ig_scaled)/3) - scaled_coef).mean()}")
+    print("")
+
+    point_five = np.full((NUM_FEATS), 0.5)
+    zeros = np.full((NUM_FEATS), 0)
+    ones = np.full((NUM_FEATS), 1)
+    print(f"Dummy Average of 0.0 error: {np.abs(zeros - scaled_coef).mean()}")
+    print(f"Dummy Average of 0.5 error: {np.abs(point_five - scaled_coef).mean()}")
+    print(f"Dummy Average of 0.1 error: {np.abs(ones - scaled_coef).mean()}")
+    print("")
     
     # median
     total_scaled_median = np.median(all_stacked, axis=1)
@@ -370,59 +483,15 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
 
     total_scaled_mode = np.reshape(total_scaled_mode, (-1,))
 
-
-    # helper function for spearman and kendall
-    def helper_rank(rank_function, data, methods_columns_name):
-
-        # calculate rank correlation and p-value. null hypothesis is that two sets of data are uncorrelated
-        all_corr = np.zeros((data.shape[1], data.shape[1]))
-        all_p_value = np.zeros((data.shape[1], data.shape[1]))
-        for i in range(data.shape[1]):
-            for j in range(data.shape[1]):
-                corr, p_value = rank_function(data[:, i], data[:, j])
-                all_corr[i, j] = corr
-                all_p_value[i, j] = p_value
-
-        # store correlation and p-value in dataframe
-        df_corr = pd.DataFrame(all_corr, columns=methods_columns_name, index=methods_columns_name)
-        df_p_value = pd.DataFrame(all_p_value, columns=methods_columns_name, index=methods_columns_name)
-
-        # generate truth table for p-value. If p value < 0.05(significant) value is True/1.0
-        df_p_value_significance = df_p_value < 0.05
-
-        # remove diagonal significance as it is measured against itself
-        for i in range(df_p_value.shape[0]):
-            df_p_value_significance.iloc[i, i] = None
-
-        # decide if feature importance method should be kept
-        # if majority are correlated and statistically significant then method are kept
-        keep_FI_method = []
-        keep_or_not = True
-
-        # zeros placeholder same number of rows/feature
-        all_stacked_corr = np.zeros((data.shape[0], 1))
-
-        # loop to include only majority voted method. Majority voted in this case is equivalent
-        # to all method deemed correlated by statistical significance
-        for i in range(df_p_value_significance.shape[1]):
-            keep_or_not = Counter(df_p_value_significance.iloc[:, i]).most_common()[0][0]
-            keep_FI_method.append(keep_or_not)
-            if keep_or_not:
-                all_stacked_corr = np.hstack((all_stacked_corr, np.reshape(data[:, i], (-1, 1))))
-
-        # remove the zeros placeholder for shape
-        all_stacked_corr = all_stacked_corr[:, 1:]
-
-        # calculate mean for each feature
-        total_scaled_corr = np.mean(all_stacked_corr, axis=1)
-        return total_scaled_corr, df_p_value
+    # majority vote
+    total_scaled_majority_vote = majority_vote_func(all_stacked, NUM_FEATS)
 
     # kendall tau
     columns_name = ["rf_sv", "rf_pi", "gb_sv", "gb_pi", "dnn_pi", "dnn_sv", "dnn_ig"]
-    total_scaled_kendall_tau, kendall_tau_p_value = helper_rank(stats.kendalltau, all_stacked, columns_name)
+    total_scaled_kendall_tau, kendall_tau_p_value = helper_rank_func(stats.kendalltau, all_stacked, columns_name, NUM_FEATS)
 
     # spearman
-    total_scaled_spearman, spearman_p_value = helper_rank(stats.spearmanr, all_stacked, columns_name)
+    total_scaled_spearman, spearman_p_value = helper_rank_func(stats.spearmanr, all_stacked, columns_name, NUM_FEATS)
 
     # convert signifance to true or false
     def plot_rank_method_significance(p_value, rank_name, sig_threshold, columns_name):
@@ -445,55 +514,14 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
         )
 
         fig.update_layout(
-            title_text=f"Are association between different feature importance methods and models statistically significant? ({rank_name})"
+            title_text=f"Are association between different feature importance methods and models statistically significant? ({rank_name}) Informative Features: {informative}, Noise level: {noise}"
         )
-        fig.show()
+        #fig.show()
+        fig.write_html(f"Images/{rank_name}_results_{informative}_feat_{noise}_noise.html")
 
 
     plot_rank_method_significance(kendall_tau_p_value, "Kendall Tau", 0.05, columns_name)
     plot_rank_method_significance(spearman_p_value, "Spearman Rho", 0.05,columns_name)
-
-
-    # obtain rank of each feature for each feature importance
-    all_stacked_rank = np.zeros((20, 1))
-    for i in range(all_stacked.shape[1]):
-        all_stacked_rank = np.hstack((all_stacked_rank, np.reshape(np.argsort(all_stacked[:, i]), (-1, 1))))
-
-    all_stacked_rank = all_stacked_rank[:, 1:]
-
-    # select the most common rank for each feature
-    most_common_rank = []
-    for i in range(all_stacked_rank.shape[0]):
-
-        majority_number = Counter(all_stacked_rank[i, :]).most_common()[0][1]
-        second_majority_number = Counter(all_stacked_rank[i, :]).most_common()[1][1]
-        # record instances if there are two majority, else just append the majority
-        if (majority_number) == (second_majority_number):
-            most_common_rank.append(
-                [
-                    int(Counter(all_stacked_rank[i, :]).most_common()[0][0]),
-                    int(Counter(all_stacked_rank[i, :]).most_common()[1][0]),
-                ]
-            )
-        else:
-            most_common_rank.append(([int(Counter(all_stacked_rank[i, :]).most_common()[0][0])]))
-
-    # average of majorty
-    total_scaled_majority_vote = list()
-    for i in range(all_stacked_rank.shape[0]):
-        temp_majority_avg = []
-        for j in range(all_stacked_rank.shape[1]):
-            # single majority
-            if len(most_common_rank[i]) == 1:
-                if all_stacked_rank[i, j] == most_common_rank[i][0]:
-                    temp_majority_avg.append(all_stacked[i, j])
-            # double majority
-            else:
-                if (all_stacked_rank[i, j] == most_common_rank[i][0]) or (all_stacked_rank[i, j] == most_common_rank[i][1]):
-                    temp_majority_avg.append(all_stacked[i, j])
-        total_scaled_majority_vote.append(mean(temp_majority_avg))
-
-    total_scaled_majority_vote = np.array(total_scaled_majority_vote)
 
     # renormalise the result
 
@@ -572,7 +600,7 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
     )
 
 
-    df_results.to_csv("results.csv")
+    df_results.to_csv(f"Tabular/results_{informative}_feat_{noise}_noise.csv")
 
     fig = px.scatter(df_results, x="Features", y="Importance", color="Methods")
 
@@ -582,5 +610,6 @@ def ensemble_feature_importance(NUM_FEATS, features, output, coef):
 
     fig.update_traces(marker=dict(size=12, line=dict(width=2, color="Black")), selector=dict(mode="markers"), opacity=0.7)
 
-    fig.write_html("results.html")
+    fig.write_html(f"Images/scatter_results_{informative}_feat_{noise}_noise.html")
     fig.show()
+    
